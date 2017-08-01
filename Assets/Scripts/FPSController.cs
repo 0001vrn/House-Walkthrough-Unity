a6f3753b8cd28511f1ca94771de1616a71cf41c0
BSW123 <bsw123@Anupams-MacBook-Pro.local>
2017-08-01 17:51:22 +0530

    first commit

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour {
+
+    public float speed = 2;
+    bool moveFB = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+
+        if (Input.GetMouseButtonDown(0))
+            moveFB = true;
+
+        if (Input.GetMouseButtonUp(0))
+            moveFB = false;
+
+        MoveForward();
+
+
+
+    }
+
+    void MoveForward(){
+
+
+        if (!moveFB)
+            return;
+
+        Vector3 movement = new Vector3(0, 0, speed);
+
+        transform.Translate(movement*Time.deltaTime);
+
+    }
+
+}
---
+++ Assets/Scripts/RotationalTracking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class RotationalTracking : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        transform.localEulerAngles = new Vector3(Camera.main.transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, 0);
+	}
+}
